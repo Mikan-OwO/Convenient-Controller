598475fa53c06715e39c484d26866da6529efb4d
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-26 00:03:47 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-exports.controller = class Controller {
+const { Client } = require("discord.js");
+
+exports.Controller = class Controller {
+  constructor(client: Client) {
+  this.client = client
+  }
+  
+  addPage(embed) {
+    this.pages.set(this.pages.size, embed)
+    return this
+  }
 }
