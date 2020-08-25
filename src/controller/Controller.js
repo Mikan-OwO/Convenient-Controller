cd26eb3c819c2a2fce96d6744355f7ceccda483d
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-25 23:34:10 +0900

    Update Controller.js

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,20 +1,5 @@
-'use strict'
-
-const fs = require("fs");
-
-class Field {
-  constructor() {
-    this.tiles = [
-    [1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1],
-    ];
+class ReactionController {
+  constructor(options = {}) {
   }
-  let x = this.tiles[0].length;
-  let y = this.tiles.length;
-if(x < 0 || x > 6 || y < 0 || y > 6) return 1;
+  this.addPage = ""
 }
